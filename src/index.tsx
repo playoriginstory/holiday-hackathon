a1e0a67833af27dab7656b183926fb9d7f541682
originstory <seancarroll@Seans-MacBook-Pro.local>
2025-01-07 12:02:00 +0100

    tsconfig fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/App';
+import App from './pages/app';
 
 // React 18+ uses `createRoot` for rendering
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
